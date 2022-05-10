623814ba706fbbb5ee0aa304674ee08ea19e3ad8
Ivan Holguin <andres85044@gmail.com>
2022-05-10 21:33:56 +0000

    Updating project

---
+++ webpack.common.js
@@ -0,0 +1,73 @@
+const path = require("path");
+
+// Plugins import
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// -------------------------------
+// Individual rules config
+
+const rulesForAssets = {
+  test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  exclude: "/node_modules",
+  type: "asset/resource",
+};
+
+const rulesForSassStyles = {
+  test: /\.scss$/i,
+  exclude: "/node_modules",
+  use: [
+    MiniCssExtractPlugin.loader,
+    {
+      loader: "css-loader",
+      options: {
+        sourceMap: false,
+      },
+    },
+    {
+      loader: "sass-loader",
+      options: {
+        sourceMap: false,
+      },
+    },
+  ],
+};
+
+// -------------------------------
+
+// -------------------------------
+// Plugins config
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    showErrors: true,
+    template: "./src/index.html",
+  }),
+  new MiniCssExtractPlugin({
+    filename: "[name][contenthash].bundle.css",
+  }),
+];
+
+// -------------------------------
+
+const rules = [rulesForSassStyles, rulesForAssets];
+
+module.exports = {
+  entry: "./src/js/App.js",
+  output: {
+    filename: "[name][contenthash].bundle.js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+
+  module: { rules },
+
+  resolve: {
+    alias: {
+      Images: path.resolve(__dirname, "src/assets/images"),
+      Styles: path.resolve(__dirname, "src/scss/app.scss"),
+    },
+  },
+
+  plugins: plugins,
+};
---
+++ webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+
+  plugins: [new CssMinimizerPlugin(), new TerserPlugin()],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        minify: TerserPlugin.uglifyJsMinify,
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+});
