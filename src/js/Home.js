f1c362eb1c44d9fcbf8cd2cedf57e6f3ca493f42
Ascecent <andres85044@gmail.com>
2022-05-18 02:34:41 -0500

    Fixing firebase home invocation

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,9 +1,17 @@
 import {
+    initializeApp
+} from 'firebase/app';
+
+import {
     getAuth,
     signOut
 } from "firebase/auth";
 
-const auth = getAuth();
+import config from "./FirebaseConfig"
+
+const app = initializeApp(config)
+const auth = getAuth()
+
 signOut(auth).then(() => {
     window.location.href = './../index.html'
 }).catch((error) => {
